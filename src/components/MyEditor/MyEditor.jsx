b2f9b5e9dca89401020b92ce6cb1320283fb0f60
현태수 <qvil@op.gg>
2019-05-28 17:56:54 +0900

    Add draft-js-image-plugin

--- src/components/MyEditor/MyEditor.jsx
+++ src/components/MyEditor/MyEditor.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
-import {
-  Editor,
-  EditorState,
-  RichUtils,
-  convertFromRaw,
-  convertToRaw
-} from "draft-js";
+import { EditorState, RichUtils, convertFromRaw, convertToRaw } from "draft-js";
+import Editor from "draft-js-plugins-editor";
+import createImagePlugin from "draft-js-image-plugin";
 import {
   Container,
   Preview,
@@ -14,13 +10,62 @@ import {
   EditorStyle
 } from "./MyEditor.style";
 
+const imagePlugin = createImagePlugin();
+const initialState = {
+  entityMap: {
+    "0": {
+      type: "IMAGE",
+      mutability: "IMMUTABLE",
+      data: {
+        src: "//ddragon.leagueoflegends.com/cdn/9.10.1/img/champion/Braum.png"
+      }
+    }
+  },
+  blocks: [
+    {
+      key: "9gm3s",
+      text:
+        "You can have images in your text field. This is a very rudimentary example, but you can enhance the image plugin with resizing, focus or alignment plugins.",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {}
+    },
+    {
+      key: "ov7r",
+      text: " ",
+      type: "atomic",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [
+        {
+          offset: 0,
+          length: 1,
+          key: 0
+        }
+      ],
+      data: {}
+    },
+    {
+      key: "e23a8",
+      text: "See advanced examples further down …",
+      type: "unstyled",
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {}
+    }
+  ]
+};
+
 const MyEditor = () => {
   const [editorState, setEditorState] = useState(
     localStorage.editorState
       ? EditorState.createWithContent(
           convertFromRaw(JSON.parse(localStorage.editorState))
         )
-      : EditorState.createEmpty()
+      : EditorState.createWithContent(convertFromRaw(initialState))
   );
   const [text, setText] = useState("Preview");
 
@@ -54,6 +99,7 @@ const MyEditor = () => {
       <EditorStyle>
         <Editor
           editorState={editorState}
+          plugins={[imagePlugin]}
           onChange={onChange}
           handleKeyCommand={handleKeyCommand}
           placeholder="Typing here"
